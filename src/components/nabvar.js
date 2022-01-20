05db6747c7d6cdabb48f79b94162d3cefcc83ef0
daneydag <dgonzalez9413@richmondhillhs.org>
2022-01-20 01:47:25 +0000

    build-a-blog

---
+++ src/components/nabvar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Navbar() {
+  return (
+    <div>
+      This is the Navbar
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/components/site.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Site() {
+  return (
+    <div>
+      This is the Site
+    </div>
+  );
+}
+
+export default Site;
